991224ad7bf845058522f819a5862b806fcef5f3
KisCode <keno123@126.com>
2016-03-16 21:13:57 +0800

    初次提交

--- DynamicLoadApk/sample/depend_on_interface/doi-host/src/com/ryg/dynamicload/sample/doihost/MainActivity.java
+++ DynamicLoadApk/sample/depend_on_interface/doi-host/src/com/ryg/dynamicload/sample/doihost/MainActivity.java
@@ -25,144 +25,162 @@ import com.ryg.dynamicload.sample.docommon.HostInterfaceManager;
 import com.ryg.dynamicload.sample.doihost.interfacee.HostInterfaceImp;
 import com.ryg.utils.DLUtils;
 
+/***
+ * 初次提交
+ * 
+ * @author
+ * 
+ */
 public class MainActivity extends Activity implements OnItemClickListener {
 
-    public static final String FROM = "extra.from";
-    public static final int FROM_INTERNAL = 0;
-    public static final int FROM_EXTERNAL = 1;
+	public static final String FROM = "extra.from";
+	public static final int FROM_INTERNAL = 0;
+	public static final int FROM_EXTERNAL = 1;
 
-    private ArrayList<PluginItem> mPluginItems = new ArrayList<PluginItem>();
-    private PluginAdapter mPluginAdapter;
+	private ArrayList<PluginItem> mPluginItems = new ArrayList<PluginItem>();
+	private PluginAdapter mPluginAdapter;
 
-    private ListView mListView;
-    private TextView mNoPluginTextView;
+	private ListView mListView;
+	private TextView mNoPluginTextView;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        initView();
-        initData();
-        
-        HostInterfaceManager.setHostInterface(new HostInterfaceImp());
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		initView();
+		initData();
 
-    private void initView() {
-        mPluginAdapter = new PluginAdapter();
-        mListView = (ListView) findViewById(R.id.plugin_list);
-        mNoPluginTextView = (TextView)findViewById(R.id.no_plugin);
-    }
+		HostInterfaceManager.setHostInterface(new HostInterfaceImp());
+	}
 
-    private void initData() {
-        String pluginFolder = Environment.getExternalStorageDirectory() + "/DynamicLoadHost";
-        File file = new File(pluginFolder);
-        File[] plugins = file.listFiles();
-        if (plugins == null || plugins.length == 0) {
-            mNoPluginTextView.setVisibility(View.VISIBLE);
-            return;
-        }
+	private void initView() {
+		mPluginAdapter = new PluginAdapter();
+		mListView = (ListView) findViewById(R.id.plugin_list);
+		mNoPluginTextView = (TextView) findViewById(R.id.no_plugin);
+	}
 
-        for (File plugin : plugins) {
-            PluginItem item = new PluginItem();
-            item.pluginPath = plugin.getAbsolutePath();
-            item.packageInfo = DLUtils.getPackageInfo(this, item.pluginPath);
-            mPluginItems.add(item);
-            DLPluginManager.getInstance(this).loadApk(item.pluginPath);
-        }
+	private void initData() {
+		String pluginFolder = Environment.getExternalStorageDirectory()
+				+ "/DynamicLoadHost";
+		File file = new File(pluginFolder);
+		File[] plugins = file.listFiles();
+		if (plugins == null || plugins.length == 0) {
+			mNoPluginTextView.setVisibility(View.VISIBLE);
+			return;
+		}
 
-        mListView.setAdapter(mPluginAdapter);
-        mListView.setOnItemClickListener(this);
-        mPluginAdapter.notifyDataSetChanged();
-    }
+		for (File plugin : plugins) {
+			PluginItem item = new PluginItem();
+			item.pluginPath = plugin.getAbsolutePath();
+			item.packageInfo = DLUtils.getPackageInfo(this, item.pluginPath);
+			mPluginItems.add(item);
+			DLPluginManager.getInstance(this).loadApk(item.pluginPath);
+		}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+		mListView.setAdapter(mPluginAdapter);
+		mListView.setOnItemClickListener(this);
+		mPluginAdapter.notifyDataSetChanged();
+	}
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-        case R.id.action_settings:
-            DLUtils.showDialog(this, getString(R.string.action_about), getString(R.string.introducation));
-            break;
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
 
-        default:
-            break;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.action_settings:
+			DLUtils.showDialog(this, getString(R.string.action_about),
+					getString(R.string.introducation));
+			break;
 
-    private class PluginAdapter extends BaseAdapter {
+		default:
+			break;
+		}
+		return super.onOptionsItemSelected(item);
+	}
 
-        private LayoutInflater mInflater;
+	private class PluginAdapter extends BaseAdapter {
 
-        public PluginAdapter() {
-            mInflater = MainActivity.this.getLayoutInflater();
-        }
+		private LayoutInflater mInflater;
 
-        @Override
-        public int getCount() {
-            return mPluginItems.size();
-        }
+		public PluginAdapter() {
+			mInflater = MainActivity.this.getLayoutInflater();
+		}
 
-        @Override
-        public Object getItem(int position) {
-            return mPluginItems.get(position);
-        }
+		@Override
+		public int getCount() {
+			return mPluginItems.size();
+		}
 
-        @Override
-        public long getItemId(int position) {
-            return position;
-        }
+		@Override
+		public Object getItem(int position) {
+			return mPluginItems.get(position);
+		}
 
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            ViewHolder holder;
-            if (convertView == null) {
-                convertView = mInflater.inflate(R.layout.plugin_item, parent, false);
-                holder = new ViewHolder();
-                holder.appIcon = (ImageView) convertView.findViewById(R.id.app_icon);
-                holder.appName = (TextView) convertView.findViewById(R.id.app_name);
-                holder.apkName = (TextView) convertView.findViewById(R.id.apk_name);
-                holder.packageName = (TextView) convertView.findViewById(R.id.package_name);
-                convertView.setTag(holder);
-            } else {
-                holder = (ViewHolder) convertView.getTag();
-            }
-            PluginItem item = mPluginItems.get(position);
-            PackageInfo packageInfo = item.packageInfo;
-            holder.appIcon.setImageDrawable(DLUtils.getAppIcon(MainActivity.this, item.pluginPath));
-            holder.appName.setText(DLUtils.getAppLabel(MainActivity.this, item.pluginPath));
-            holder.apkName.setText(item.pluginPath.substring(item.pluginPath.lastIndexOf(File.separatorChar) + 1));
-            holder.packageName.setText(packageInfo.applicationInfo.packageName);
-            return convertView;
-        }
-    }
+		@Override
+		public long getItemId(int position) {
+			return position;
+		}
 
-    private static class ViewHolder {
-        public ImageView appIcon;
-        public TextView appName;
-        public TextView apkName;
-        public TextView packageName;
-    }
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			ViewHolder holder;
+			if (convertView == null) {
+				convertView = mInflater.inflate(R.layout.plugin_item, parent,
+						false);
+				holder = new ViewHolder();
+				holder.appIcon = (ImageView) convertView
+						.findViewById(R.id.app_icon);
+				holder.appName = (TextView) convertView
+						.findViewById(R.id.app_name);
+				holder.apkName = (TextView) convertView
+						.findViewById(R.id.apk_name);
+				holder.packageName = (TextView) convertView
+						.findViewById(R.id.package_name);
+				convertView.setTag(holder);
+			} else {
+				holder = (ViewHolder) convertView.getTag();
+			}
+			PluginItem item = mPluginItems.get(position);
+			PackageInfo packageInfo = item.packageInfo;
+			holder.appIcon.setImageDrawable(DLUtils.getAppIcon(
+					MainActivity.this, item.pluginPath));
+			holder.appName.setText(DLUtils.getAppLabel(MainActivity.this,
+					item.pluginPath));
+			holder.apkName.setText(item.pluginPath.substring(item.pluginPath
+					.lastIndexOf(File.separatorChar) + 1));
+			holder.packageName.setText(packageInfo.applicationInfo.packageName);
+			return convertView;
+		}
+	}
 
-    public static class PluginItem {
-        public PackageInfo packageInfo;
-        public String pluginPath;
+	private static class ViewHolder {
+		public ImageView appIcon;
+		public TextView appName;
+		public TextView apkName;
+		public TextView packageName;
+	}
 
-        public PluginItem() {
-        }
-    }
+	public static class PluginItem {
+		public PackageInfo packageInfo;
+		public String pluginPath;
 
-    @Override
-    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        PluginItem item = mPluginItems.get(position);
-        DLPluginManager pluginManager = DLPluginManager.getInstance(this);
-        pluginManager.startPluginActivity(this, new DLIntent(item.packageInfo.packageName));
-        
-    }
+		public PluginItem() {
+		}
+	}
+
+	@Override
+	public void onItemClick(AdapterView<?> parent, View view, int position,
+			long id) {
+		PluginItem item = mPluginItems.get(position);
+		DLPluginManager pluginManager = DLPluginManager.getInstance(this);
+		pluginManager.startPluginActivity(this, new DLIntent(
+				item.packageInfo.packageName));
+
+	}
 
 }
